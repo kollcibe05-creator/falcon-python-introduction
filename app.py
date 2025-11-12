d215f64c2b3a719efa3cbb4acd047a9344ba08db
Collins Kibet <kollcibe05@gmail.com>
2025-11-12 22:31:13 +0300

    fibonacci_list

--- app.py
+++ app.py
@@ -64,3 +64,20 @@ def generate_squares(n):
         print(f"Square: {square}")
         
 generate_squares(5)
+
+
+
+
+def print_fibonacci(length):
+    if length <= 0:
+        return []
+    elif length == 1:
+        return [1]
+    else:
+        fib_list = [0, 1] 
+        while len(fib_list) < length:
+            next_in_list = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_in_list)       
+        return fib_list
+
+print_fibonacci(9)    
